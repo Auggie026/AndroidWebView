f88041340b55af23a102f2d1911b027e9549ea72
Augusto Castro <augustocesarcastro026@gmail.com>
2021-06-01 22:01:12 -0300

    app done in one sit. Android Web View applied

--- app/src/main/java/com/example/androidwebview/MainActivity.kt
+++ app/src/main/java/com/example/androidwebview/MainActivity.kt
@@ -2,10 +2,73 @@ package com.example.androidwebview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
+import com.example.androidwebview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+    private val link = "https://mhrise.kiranico.com"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       /* binding.webView.apply {
+            loadUrl(link)
+        }*/
+
+       // myHtml()
+
+        turnYourWebSiteIntoAndroidApp()
+    }
+
+    private fun turnYourWebSiteIntoAndroidApp(){
+
+        binding.webView.apply {
+
+            webViewClient = WebViewClient()
+            loadUrl(link)
+
+            settings.apply {
+                javaScriptEnabled = true
+                setAppCacheEnabled(true)
+                cacheMode = WebSettings.LOAD_DEFAULT
+                setSupportZoom(false)
+                builtInZoomControls = false
+                displayZoomControls = false
+                textZoom = 100
+                blockNetworkImage = false
+                loadsImagesAutomatically = true
+            }
+        }.canGoBack()
+    }
+
+    override fun onBackPressed() {
+        if(binding.webView.canGoBack()){
+            binding.webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun myHtml(){
+
+        val customHtml = "<html><body>" +
+                "<h1>Hello There</h1>" +
+                "<h2>Heading 2</h2>" +
+                "<h3>Heading 3</h3>" +
+                "<p>This is a sample <br>paragraph of your</br> custom Html...</p>" +
+                "</html></body>"
+
+
+        binding.webView.apply {
+            loadData(customHtml,
+            "text/html",
+            "UTF-8")
+        }
     }
 }
